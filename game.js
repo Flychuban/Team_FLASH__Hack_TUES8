583e4db20816d014933362449e61d5002658c022
Vasko1234 <vasilvelev07@gmail.com>
2022-03-11 02:05:28 +0200

    jump animation

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const statrElem=document.querySelector("[data-start-screen]");
+var robot = document.getElementById("robot");
 
 document.addEventListener("keydown",function(event){
     remove();
@@ -6,4 +7,18 @@ document.addEventListener("keydown",function(event){
 
 function remove(){
     statrElem.classList.add("hide"); 
+}
+
+document.addEventListener("keydown", () => {
+    jump();
+});
+
+function jump() {
+    if (robot.classList != "robot_jump")
+    {
+        robot.classList.add("robot_jump");
+        setTimeout( () => {
+            robot.classList.remove("robot_jump");
+        }, 500);
+    }
 }
